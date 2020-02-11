9214a02ff348b2cddcade330d4b46cf13575aa52
Ahmed Ayoub <great.nile@gmail.com>
2020-02-11 21:45:00 +0100

    add another usecase example

--- nile.py
+++ nile.py
@@ -5,10 +5,10 @@ except ImportError:
     from yaml import Loader, Dumper
 
 # open yaml file
-file = open('data_without_hobbies.yaml', 'r')
+file = open('data.yaml', 'r')
 
 # prepare output file
-output_file = open('data_with_hobbies.yaml', 'w')
+output_file = open('data_modified.yaml', 'w')
 
 # read and parse the yaml data
 data = load(file.read(), Loader=Loader)
@@ -17,6 +17,11 @@ data = load(file.read(), Loader=Loader)
 if 'hobbies' not in data:
     data['hobbies'] = ['reading', 'developing']
     print(data)
+
+# add a friend if it is not already there
+if 'friends' in data:
+    if 'ahmed' not in data['friends']: 
+        data['friends'].append('ahmed')
 
 # save the newly modified yaml data to different file
 dump(data, output_file)
